d172962efe1756c20dcb4d22b3ad06e07f0e6846
mikedevv <maungdevv@gmail.com>
2024-11-21 23:07:38 +0700

    fix: duration to 1

--- src/components/OverlayText.jsx
+++ src/components/OverlayText.jsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion"
 const OverlayText = ({name,amount}) => {
   return (
     <div>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: {duration: .2} }} className='text-yellow-500 text-xs z-10 relative bottom-[7.3rem] left-[5.6rem] font-semibold w-[10rem] text-center'>{amount}ကျပ််</motion.div>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: {duration: .2} }} className='text-yellow-500 text-xs z-10 relative bottom-[6rem] left-[5.6rem] font-semibold w-[10rem] text-center'>{name}</motion.div>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: {duration: 1} }} className='text-yellow-500 text-xs z-10 relative bottom-[7.3rem] left-[5.6rem] font-semibold w-[10rem] text-center'>{amount}ကျပ််</motion.div>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: {duration: 1} }} className='text-yellow-500 text-xs z-10 relative bottom-[6rem] left-[5.6rem] font-semibold w-[10rem] text-center'>{name}</motion.div>
     </div>
   )
 }
