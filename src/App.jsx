fe2bd5d7449781529f5a7d23227b771e49d3ecb1
mikedevv <maungdevv@gmail.com>
2024-11-23 00:56:51 +0700

    feat: err element

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,11 @@ import Certificate from "./pages/Certificate";
 import Home from "./pages/Home";
 import Root from "./pages/Root";
 import ViewPdf from "./pages/ViewPdf";
+import Err from "./pages/Err";
 // You can do this:
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Root />}>
+    <Route errorElement={<Err />} path="/" element={<Root />}>
       <Route index element={<Home />} />
       <Route path="certificate" element={<Certificate />} />
       <Route path="certificate/view" element={<ViewPdf />} />
---
+++ src/pages/Err.jsx
@@ -0,0 +1,10 @@
+
+const  Err= () => {
+  return (
+    <div className='text-xl font-bold text-blue-600 h-screen w-screen flex justify-center items-center'>
+        <span>Something went wrong!</span>
+    </div>
+  )
+}
+
+export default Err
